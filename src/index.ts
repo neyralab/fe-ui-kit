049dc14a6e0f7724a4e3f62b6b5fffbef6630ed6
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-04-17 20:27:43 +0300

    -import theme css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import './styles/dark-theme.css';
+import './styles/light-theme.css';
+
 import Border from './components/Border';
 import Button from './components/Button';
 import Input from './components/Input';
