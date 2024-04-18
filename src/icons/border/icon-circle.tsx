cc853263247f16d038b7ebdc89e74cba0bc9ee7d
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-04-18 17:41:29 +0300

    -added border icons

---
+++ src/icons/border/icon-circle.tsx
@@ -0,0 +1,15 @@
+const IconCircle = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <circle cx="7" cy="7" r="7" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default IconCircle;
---
+++ src/icons/border/index.ts
@@ -0,0 +1,23 @@
+import IconChat from './icon-chat';
+import IconClear from './icon-clear';
+import IconDrive from './icon-drive';
+import IconLotus from './icon-lotus';
+import IconMoon from './icon-moon';
+import IconRecord from './icon-record';
+import IconSettings from './icon-settings';
+import IconSun from './icon-sun';
+import IconCircle from './icon-circle';
+import IconSpeaker from './icon-speaker';
+
+export {
+  IconChat,
+  IconClear,
+  IconDrive,
+  IconLotus,
+  IconMoon,
+  IconRecord,
+  IconSettings,
+  IconSun,
+  IconCircle,
+  IconSpeaker,
+};
